29f985a986a94ed3aec640738d9b6486e650d211
TomasZamastil <thomas.zamastil@gmail.com>
2023-11-14 14:56:34 +0100

    App Skeleton from Spring Initializr
    Original Commit of the skeleton of the app generated at start.spring.io

---
+++ onlineBankovnictviApp/src/main/java/com/tzamastil/onlineBankovnictviApp/OnlineBankovnictviAppApplication.java
@@ -0,0 +1,13 @@
+package com.tzamastil.onlineBankovnictviApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBankovnictviAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineBankovnictviAppApplication.class, args);
+	}
+
+}
---
+++ onlineBankovnictviApp/src/test/java/com/tzamastil/onlineBankovnictviApp/OnlineBankovnictviAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tzamastil.onlineBankovnictviApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBankovnictviAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
