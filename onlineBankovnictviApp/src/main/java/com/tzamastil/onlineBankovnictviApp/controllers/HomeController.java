84a69e198a0e1afd31892d41fad97e81f8428fe3
TomasZamastil <thomas.zamastil@gmail.com>
2023-11-20 22:30:39 +0100

    mini update for home page controller

--- onlineBankovnictviApp/src/main/java/com/tzamastil/onlineBankovnictviApp/controllers/HomeController.java
+++ onlineBankovnictviApp/src/main/java/com/tzamastil/onlineBankovnictviApp/controllers/HomeController.java
@@ -6,7 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
-    @RequestMapping("/home")
+    @RequestMapping({"/home", "", "/"})
+
     public String getHome() {
 
         return "home/homePage";
